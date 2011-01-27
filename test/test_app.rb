c22d99264a4372bb5c4e6e9bba635b265a7c721e
Jeffrey Basurto <jeffreybasurto@gmail.com>
2011-01-27 02:27:34 -0600

    Added more callback support.

--- test/test_app.rb
+++ test/test_app.rb
@@ -13,10 +13,11 @@ ah = Auctioneer::House.first_or_create(:name=>"alliance")
 
 # When an auction expires yield on this block.
 ah.after_expire do |ticket|
-  puts "Ticket "
+  puts "Ticket #{ticket.id} has expired."
 end
 
 ah.after_complete do |ticket|
+  puts "Ticket #{ticket.id} has completed successfully."
 end
 
 ah.tickets.destroy!
@@ -24,4 +25,5 @@ ah.create_auction(Item.new("fancy bracelet"), {:owner=>"Jeffrey Basurto", :curre
 ah.create_auction(Item.new("crown"),  {:owner=>"Jeffrey Basurto", :current_bid=>70})
 ah.create_auction(Item.new("red fedora"), {:owner=>"Jonathan"})
 
+# query for all of Jefffrey Basurto's auctions that the current bid is at more than 60.
 ah.all({:owner=>"Jeffrey Basurto".to_yaml, :current_bid.gt=>60}).each {|t| puts "Item: #{t.item.name}  Owner: #{t.owner}" }
